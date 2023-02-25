0165be8349419b42f547520d1f01c5b46a7d52ce
RAHUL-KAD <kadamrahulj0909@gmail.com>
2023-02-25 23:41:17 +0530

    updated username in git cli

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
     <div className='sticky top-0 w-full left-0 flex items-center justify-between p-6'>
         <h1 className='text-3xl sm:text-6xl'>TODO LIST</h1>
         <i className='fa-solid fa-user text-xl sm:text-4xl'/>
+        
     </div>
   )
 }
